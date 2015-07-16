7783a60c4ee8ba6c18c95278ad633207f8ad2b69
trod-123 <cia.123trod@gmail.com>
2015-07-16 14:23:26 -0700

    Days Counter: Use looping to increase efficiency of constructing prospective dates table.

--- android/DaysCounter/app/src/main/java/com/thirdarm/dayscounter/MainActivity.java
+++ android/DaysCounter/app/src/main/java/com/thirdarm/dayscounter/MainActivity.java
@@ -23,6 +23,12 @@ public class MainActivity extends Activity {
     public static String dateString;
     public static TextView dateView;
 
+    public static final int NUM_COLUMNS = 3;
+    public static final int NUM_DATES = 60;
+
+    public static final int TITLE_SIZE = 17;
+    public static final int DATE_SIZE = 16;
+
     private Date mDate;
 
     @Override
@@ -91,103 +97,104 @@ public class MainActivity extends Activity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
             TableLayout mDateTable = (TableLayout) rootView.findViewById(R.id.prospectiveCounter);
-            final int titleSize = 17;
-            final int textSize = 16;
 
             // play with dates
             Calendar c = Calendar.getInstance();
             c.add(Calendar.DAY_OF_MONTH, -1); // so that day 1 includes current date
 
-            // title row
+            // generate title row
             TableRow titleRow = new TableRow(getActivity());
 
-            TextView day = new TextView(getActivity());
-            day.setText("Count");
-            day.setTextSize(titleSize);
-            day.setPadding(20, 10, 20, 10);
-            titleRow.addView(day);
+            for (int i = 0; i < NUM_COLUMNS; i++) {
+                TextView day = new TextView(getActivity());
+                day.setText("Count");
+                day.setTextSize(TITLE_SIZE);
+                day.setPadding(20, 10, 20, 10);
+                titleRow.addView(day);
 
-            TextView proDate = new TextView(getActivity());
-            proDate.setText("Date");
-            proDate.setTextSize(titleSize);
-            proDate.setPadding(20, 10, 40, 10);
-            titleRow.addView(proDate);
-
-            TextView day2 = new TextView(getActivity());
-            day2.setText("Count");
-            day2.setTextSize(titleSize);
-            day2.setPadding(40, 10, 20, 10);
-            titleRow.addView(day2);
-
-            TextView proDate2 = new TextView(getActivity());
-            proDate2.setText("Date");
-            proDate2.setTextSize(titleSize);
-            proDate2.setPadding(20, 10, 40, 10);
-            titleRow.addView(proDate2);
-
-            TextView day3 = new TextView(getActivity());
-            day3.setText("Count");
-            day3.setTextSize(titleSize);
-            day3.setPadding(40, 10, 20, 10);
-            titleRow.addView(day3);
-
-            TextView proDate3 = new TextView(getActivity());
-            proDate3.setText("Date");
-            proDate3.setTextSize(titleSize);
-            proDate3.setPadding(20, 10, 40, 10);
-            titleRow.addView(proDate3);
+                TextView proDate = new TextView(getActivity());
+                proDate.setText("Date");
+                proDate.setTextSize(TITLE_SIZE);
+                proDate.setPadding(20, 10, 40, 10);
+                titleRow.addView(proDate);
+            }
 
             mDateTable.addView(titleRow);
 
-            // make rows
+            //
+            // generate prospective date rows
+            //
+
+            // generate and store dates in a list
             ArrayList<String> dates = new ArrayList<>();
-            for (int i = 0; i < 60; i++) {
+            for (int i = 0; i < NUM_DATES; i++) {
                 c.add(Calendar.DAY_OF_MONTH, 1);
                 dates.add((c.get(Calendar.MONTH)+1) + "/" + c.get(Calendar.DAY_OF_MONTH));
             }
 
-            for (int i = 0; i < 20; i++) {
+            // print dates into table
+            int DATES_PER_COLUMN = NUM_DATES/NUM_COLUMNS;
+
+            for (int i = 0; i < DATES_PER_COLUMN; i++) {
 
                 TableRow row = new TableRow(getActivity());
                 //row.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
 
-                TextView counter = new TextView(getActivity());
-                counter.setText((i+1) + "");
-                counter.setTextSize(textSize);
-                counter.setPadding(20, 5, 20, 5);
-                row.addView(counter);
+                for (int j = 0; j < NUM_COLUMNS; j++) {
+                    TextView counter = new TextView(getActivity());
+                    counter.setText((i+1+j*DATES_PER_COLUMN) + "");
+                    counter.setTextSize(DATE_SIZE);
+                    counter.setPadding(20, 5, 20, 5);
+                    row.addView(counter);
 
-                TextView pd = new TextView(getActivity());
-                pd.setText(dates.get(i));
-                pd.setTextSize(textSize);
-                pd.setPadding(20, 5, 40, 5);
-                row.addView(pd);
+                    TextView pd = new TextView(getActivity());
+                    pd.setText(dates.get(i + j * DATES_PER_COLUMN));
+                    pd.setTextSize(DATE_SIZE);
+                    pd.setPadding(20, 5, 40, 5);
+                    row.addView(pd);
+                }
 
-                TextView counter2 = new TextView(getActivity());
-                counter2.setText((i+21) + "");
-                counter2.setTextSize(textSize);
-                counter2.setPadding(40, 5, 20, 5);
-                row.addView(counter2);
 
-                TextView pd2 = new TextView(getActivity());
-                pd2.setText(dates.get(i+20));
-                pd2.setTextSize(textSize);
-                pd2.setPadding(20, 5, 40, 5);
-                row.addView(pd2);
 
-                TextView counter3 = new TextView(getActivity());
-                counter3.setText((i+41) + "");
-                counter3.setTextSize(textSize);
-                counter3.setPadding(40, 5, 20, 5);
-                row.addView(counter3);
-
-                TextView pd3 = new TextView(getActivity());
-                pd3.setText(dates.get(i+40));
-                pd3.setTextSize(textSize);
-                pd3.setPadding(20, 5, 40, 5);
-                row.addView(pd3);
+//                TextView counter = new TextView(getActivity());
+//                counter.setText((i+1) + "");
+//                counter.setTextSize(DATE_SIZE);
+//                counter.setPadding(20, 5, 20, 5);
+//                row.addView(counter);
+//
+//                TextView pd = new TextView(getActivity());
+//                pd.setText(dates.get(i));
+//                pd.setTextSize(DATE_SIZE);
+//                pd.setPadding(20, 5, 40, 5);
+//                row.addView(pd);
+//
+//                TextView counter2 = new TextView(getActivity());
+//                counter2.setText((i+21) + "");
+//                counter2.setTextSize(DATE_SIZE);
+//                counter2.setPadding(40, 5, 20, 5);
+//                row.addView(counter2);
+//
+//                TextView pd2 = new TextView(getActivity());
+//                pd2.setText(dates.get(i+20));
+//                pd2.setTextSize(DATE_SIZE);
+//                pd2.setPadding(20, 5, 40, 5);
+//                row.addView(pd2);
+//
+//                TextView counter3 = new TextView(getActivity());
+//                counter3.setText((i+41) + "");
+//                counter3.setTextSize(DATE_SIZE);
+//                counter3.setPadding(40, 5, 20, 5);
+//                row.addView(counter3);
+//
+//                TextView pd3 = new TextView(getActivity());
+//                pd3.setText(dates.get(i+40));
+//                pd3.setTextSize(DATE_SIZE);
+//                pd3.setPadding(20, 5, 40, 5);
+//                row.addView(pd3);
 
                 mDateTable.addView(row);
+
+
             }
 
             return rootView;
